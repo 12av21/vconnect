9845939dffa8cd77ab94a6ef3eb3a94b10c0ab42
Adarsh verma <adarshverma7376@gmail.com>
2025-04-05 06:58:38 +0530

    Update firebaseConfig.js

--- config/firebaseConfig.js
+++ config/firebaseConfig.js
@@ -1,11 +1,6 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyBAZ3Zap2Q_KBCtb6OZzdmLfARO6T0lMLg",
-  authDomain: "v-connect-7d896.firebaseapp.com",
-  projectId: "v-connect-7d896",
-  storageBucket: "v-connect-7d896.firebasestorage.app",
-  messagingSenderId: "489654796371",
-  appId: "1:489654796371:web:d0bf7b41970aec11d546a6",
-  measurementId: "G-CBRKEL7DXS"
+//enter your firebase api
+ 
 };
 
 firebase.initializeApp(firebaseConfig);
